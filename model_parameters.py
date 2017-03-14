75798296db676822cb5a105db2f90f732b22eda7
Tse Kit Yam <me@kytse.com>
2017-03-14 23:31:48 +0800

    Update model param

--- model_parameters.py
+++ model_parameters.py
@@ -1,4 +1,4 @@
-color_space = 'RGB' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+color_space = 'YCrCb' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
 orient = 9  # HOG orientations
 pix_per_cell = 8 # HOG pixels per cell
 cell_per_block = 2 # HOG cells per block
@@ -8,4 +8,4 @@ hist_bins = 16    # Number of histogram bins
 spatial_feat = True # Spatial features on or off
 hist_feat = True # Histogram features on or off
 hog_feat = True # HOG features on or off
-y_start_stop = [None, None] # Min and max in y to search in slide_window()
+y_start_stop = [400, None] # Min and max in y to search in slide_window()
